eaca0466f007e9562c248445a11efdf2ddd9f9d1
EhsanTgv <EhsanTgv@gmail.com>
2021-02-03 23:21:27 +0330

    Implement ViewBinding

--- app/src/main/java/com/taghavi/bindingpractice/MainActivity.kt
+++ app/src/main/java/com/taghavi/bindingpractice/MainActivity.kt
@@ -2,10 +2,19 @@ package com.taghavi.bindingpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.taghavi.bindingpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "Boom", Toast.LENGTH_SHORT).show()
+        }
     }
 }
